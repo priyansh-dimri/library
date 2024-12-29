5743b78e14b3fea0d3d12a67b7af1a479562a8a1
Priyansh Dimri <priyanshd0908@gmail.com>
2024-12-29 13:01:48 +0530

    add dummy data in script.js and center text in book-cards

--- script.js
+++ script.js
@@ -18,6 +18,7 @@ function addBookToLibrary(book) {
 
 function displayLibrary() {
     library.forEach((book, idx) => {
+        console.log(book, idx);
         const bookCard = document.createElement('div');
         bookCard.className = 'book-card';
 
@@ -51,3 +52,12 @@ function displayLibrary() {
         mainContainer.appendChild(bookCard);
     })
 }
+
+// Test data for Book objects
+addBookToLibrary(new Book("To Kill a thousand Mockingbird", "Harper Lee", 281, true));
+addBookToLibrary(new Book("1984", "George Orwell", 328, false));
+addBookToLibrary(new Book("The Great Gatsby", "F. Scott Fitzgerald", 180, true));
+addBookToLibrary(new Book("Harry Potter", "J. K. Rowling", 635, false));
+addBookToLibrary(new Book("Pride and Prejudice", "Jane Austen", 279, true));
+
+displayLibrary();
