76e753cd80b8c98656def0c19b16636b78139b10
Priyansh Dimri <priyanshd0908@gmail.com>
2024-12-29 12:55:26 +0530

    add displayLibrary function in script.js
    Added book read status toggle button

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 // library array contains all Book objects
 const library = [];
 
+// Main container
+const mainContainer = document.getElementsByClassName("main")[0];
+
 // Constructor function for Book object
 function Book(title, author, pages, read) {
   this.title = title;
@@ -11,4 +14,40 @@ function Book(title, author, pages, read) {
 
 function addBookToLibrary(book) {
   library.push(book);
+}
+
+function displayLibrary() {
+    library.forEach((book, idx) => {
+        const bookCard = document.createElement('div');
+        bookCard.className = 'book-card';
+
+        const bookTitle = document.createElement('div');
+        bookTitle.className = 'book-card-title';
+        bookTitle.textContent = book.title;
+        bookCard.appendChild(bookTitle);
+
+        const bookAuthor = document.createElement('div');
+        bookAuthor.className = 'book-card-author';
+        bookAuthor.textContent = `by ${book.author}`;
+        bookCard.appendChild(bookAuthor);
+
+        const bookPages = document.createElement('div');
+        bookPages.className = 'book-card-pages';
+        bookPages.textContent = `${book.pages} pages`;
+        bookCard.appendChild(bookPages);
+
+        const bookRead = document.createElement('div');
+        bookRead.className = 'book-card-read';
+        bookRead.textContent = `Read?: ${book.read ? "Yes" : "No"}`;
+        bookCard.appendChild(bookRead);
+
+        const bookToggle = document.createElement('button');
+        bookToggle.className = 'add-book-button';
+        bookToggle.type = 'button';
+        bookToggle.value = `idx-${idx}`;
+        bookToggle.textContent = "Toggle Read Status";
+        bookCard.appendChild(bookToggle);
+
+        mainContainer.appendChild(bookCard);
+    })
 }
