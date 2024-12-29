654bdceab15d4a1cd64c25f98f528aa7821dc06d
Priyansh Dimri <priyanshd0908@gmail.com>
2024-12-29 11:32:03 +0530

    auto format script.js

--- script.js
+++ script.js
@@ -3,12 +3,12 @@ const library = [];
 
 // Constructor function for Book object
 function Book(title, author, pages, read) {
-    this.title = title;
-    this.author = author;
-    this.pages = pages;
-    this.read = read;
+  this.title = title;
+  this.author = author;
+  this.pages = pages;
+  this.read = read;
 }
 
 function addBookToLibrary(book) {
-    library.push(book);
+  library.push(book);
 }
